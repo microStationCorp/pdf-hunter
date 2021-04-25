cae1bdf04e00ff70c1ae53f3ae7b1a452506c9f6
sherlock <sherlockcreation@gmail.com>
2021-04-25 12:23:13 +0530

    query primarily done

--- server/router/route.js
+++ server/router/route.js
@@ -16,4 +16,24 @@ router.get("/all", (req, res) => {
   });
 });
 
+router.get("/q", (req, res) => {
+  const regex = /[\s']/gi;
+  const keyw = req.body.key.split(regex);
+  let pat = "";
+  keyw.forEach((val, ind, arr) => {
+    if (ind == 0) {
+      pat = "(" + val + "|";
+    } else if (ind == arr.length - 1) {
+      pat = pat + val + ")";
+    } else {
+      pat = pat + val + "|";
+    }
+  });
+  PdfLink.find({ title: { $regex: pat, $options: "gi" } })
+    .then((d) => {
+      res.json(d);
+    })
+    .catch((err) => res.json(err));
+});
+
 module.exports = router;
